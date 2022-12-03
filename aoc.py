e66d2a5f7a70a6938846a69f68e8216783dd5664
kcartier <kevin.cartier@gmail.com>
2022-12-03 13:48:09 -0500

    Update aoc.py

--- aoc.py
+++ aoc.py
@@ -1,6 +1,89 @@
 ## ************************************************************************** ##
 ## **************                   FUNCTION                   ************** ##
 ## ************************************.************************************* ##
+def aoc_day_03():
+   """
+   Rucksack Reorg
+   """
+   # ---------------------------------------------------------------------------
+   log.info(">>> aoc_day_03()")
+   localpath = YAML_DOC['local_path']
+   filename = "aoc_input_day_03.txt"
+   aoc_file = open(f"{localpath}/{filename}", 'r')
+   lines = aoc_file.readlines()
+   aoc_file.close()
+
+   # lines = [ # For testing
+   #    "vJrwpWtwJgWrhcsFMMfFFhFp",
+   #    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
+   #    "PmmdzqPrVvPwwTWBwg",
+   #    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
+   #    "ttgJtRGJQctTZtZT",
+   #    "CrZsJsPPZsGzwwsLwLmpwMDw"
+   # ]
+
+   priority_values = {
+      "a":1, "b":2,   "c":3,  "d":4,  "e":5, 
+      "f":6, "g":7,   "h":8,  "i":9,  "j":10, 
+      "k":11, "l":12, "m":13, "n":14, "o":15, 
+      "p":16, "q":17, "r":18, "s":19, "t":20, 
+      "u":21, "v":22, "w":23, "x":24, "y":25, "z":26, 
+      "A":27, "B":28, "C":29, "D":30, "E":31, 
+      "F":32, "G":33, "H":34, "I":35, "J":36, 
+      "K":37, "L":38, "M":39, "N":40, "O":41, 
+      "P":42, "Q":43, "R":44, "S":45, "T":46, 
+      "U":47, "V":48, "W":49, "X":50, "Y":51, "Z":52
+   }
+
+   # ----------------------------------------------------------------------
+   # Part 1
+   # ----------------------------------------------------------------------
+   total_score = 0
+   for rucksack_contents in lines:
+      rucksack_contents = rucksack_contents.strip()
+      compartment_1 = [0] * 52
+      compartment_2 = [0] * 52
+      for i in range (0, int(len(rucksack_contents)/2)):
+         compartment_1[priority_values[rucksack_contents[i]]-1] += 1
+      for i in range (int(len(rucksack_contents)/2), len(rucksack_contents)):
+         compartment_2[priority_values[rucksack_contents[i]]-1] += 1
+      for i in range (52):
+         if compartment_1[i] != 0 and compartment_2[i] != 0:
+            total_score += i+1
+   print(f"(Part 1) Total Score: {total_score}")
+
+   # ----------------------------------------------------------------------
+   # Part 
+   # ----------------------------------------------------------------------
+   total_score = 0
+   i = 0
+   for i in range(0, len(lines), 3):
+      rucksack_contents = lines[i+0].strip()
+      rucksack_1 = [0] * 52
+      for j in range (0, len(rucksack_contents)):
+         rucksack_1[priority_values[rucksack_contents[j]]-1] += 1
+
+      rucksack_contents = lines[i+1].strip()
+      rucksack_2 = [0] * 52
+      for j in range (0, len(rucksack_contents)):
+         rucksack_2[priority_values[rucksack_contents[j]]-1] += 1
+
+      rucksack_contents = lines[i+2].strip()
+      rucksack_3 = [0] * 52
+      for j in range (0, len(rucksack_contents)):
+         rucksack_3[priority_values[rucksack_contents[j]]-1] += 1
+
+      for j in range (52):
+         if rucksack_1[j] != 0 and rucksack_2[j] != 0 and rucksack_3[j] != 0:
+            total_score += j+1
+   print(f"(Part 2) Total Score: {total_score}")
+   return
+
+
+
+## ************************************************************************** ##
+## **************                   FUNCTION                   ************** ##
+## ************************************.************************************* ##
 def aoc_day_02():
    """
    Geez, this is really hacky.  But it works.
